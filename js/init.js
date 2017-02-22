7b54fd79aac9e691e7d8c446858db933a8fd9f17
Alex Bea <alex.bea@gmail.com>
2017-02-22 17:33:34 -0500

    First commit of published styleguide.

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function (w) {
+  var sw = document.body.clientWidth,
+      sh = document.body.clientHeight;
+
+  //Update dimensions on resize
+  $(w).resize(function () {
+    sw = document.body.clientWidth;
+    sh = document.body.clientHeight;
+
+		//updateAds();
+  });
+})(this);
